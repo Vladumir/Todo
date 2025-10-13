04c1fcc90f65abee38bc0feb2a02104054d8d6d7
PC <mokoron2006@gmail.com>
2025-10-13 15:33:35 +0300

    111

---
+++ Desktop/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Desktop
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Desktop/RegistrationWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Desktop
+{
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
